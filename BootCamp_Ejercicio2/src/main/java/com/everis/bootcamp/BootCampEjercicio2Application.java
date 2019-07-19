54e19fd072085d3438ab4d592fc3d885a45d6917
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-19 15:15:10 -0500

    ejercicio 2

---
+++ BootCamp_Ejercicio2/src/main/java/com/everis/bootcamp/BootCampEjercicio2Application.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampEjercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampEjercicio2Application.class, args);
+	}
+
+}
