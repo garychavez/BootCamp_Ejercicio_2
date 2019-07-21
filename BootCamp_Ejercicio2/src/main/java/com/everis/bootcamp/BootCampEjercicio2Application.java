b93e0a787621dffa2e8daf1947506ed509b72373
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-20 21:58:17 -0500

    eureka client

--- BootCamp_Ejercicio2/src/main/java/com/everis/bootcamp/BootCampEjercicio2Application.java
+++ BootCamp_Ejercicio2/src/main/java/com/everis/bootcamp/BootCampEjercicio2Application.java
@@ -2,8 +2,10 @@ package com.everis.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BootCampEjercicio2Application {
 
 	public static void main(String[] args) {
